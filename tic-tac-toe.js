5e684bae281ca13c4fda90c4d14f78baf41bfb21
Danielle Blair <danielle2blair@gmail.com>
2022-10-12 09:27:50 -0500

    Edited tic-tac-toe to alternate putting X and O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,6 @@
 window.onload= function() {
     styleSquare();
+    insertingXandO();
 
 }
 
@@ -14,6 +15,44 @@ function styleSquare() {
             sqdivs[i].id = "id"+i;        
         }
 
+}
+
+let choices = 9;
+const entries = {id0:"", id1:"", id2:"", id3:"", id4:"", id5:"", id6:"", id7:"", id8:""};
+
+function insertingXandO(){
+    const boxes1 = document.getElementsByClassName('square');
+   
+    for (const box of boxes1) { 
+    box.addEventListener('click', function selectChoice(){
+        if ((choices>0) && ((choices%2)!=0)){
+            box.className= "square X";
+            box.textContent = "X";
+
+            for (const key in entries){
+                if (box.id == key){
+                    entries[key] = "X";
+                }
+            }
+                       
+            choices= choices - 1;
+
+        }
+        else if ((choices>0) && ((choices%2)==0)){
+            box.className= "square O";
+            box.textContent = "O";
+
+            for (const key in entries){
+                if (box.id == key){
+                    entries[key] = "O";
+                }
+            }
+
+            choices= choices -1 ;
+
+        }
+    });
+    }
 }
 
 
